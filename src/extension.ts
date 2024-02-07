0c6a98e75bd81032b3f1cb7f2f7cedc0d84a8b2f
dsult <yaroslav.vorobev-2015@mail.ru>
2024-02-07 07:10:27 -0900

    begining of refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,36 @@
 import * as vscode from 'vscode';
 
-let disposable: vscode.Disposable | undefined;
-
-export async function activate(context: vscode.ExtensionContext) {
-
-	const Parser = require('web-tree-sitter');
-	await Parser.init();
-	const parser = new Parser;
-
-	const path = require('path');
-
-	const wasmPath = path.join(__dirname, 'lib/tree-sitter-python.wasm');
-	const Python = await Parser.Language.load(wasmPath);
-	parser.setLanguage(Python);
-
-	let editor = vscode.window.activeTextEditor;
-	let tree: any;
-	if (editor) {
-		const sourceCode = editor.document.getText();
-		tree = parser.parse(sourceCode);
-	}
+interface ITypingAssist {
+	apply(tree: any, editor: vscode.TextEditor, changeEvent: vscode.TextDocumentChangeEvent): void;
+}
 
 
-
-	vscode.window.onDidChangeActiveTextEditor(e => {
-		editor = vscode.window.activeTextEditor;
-
-		if (editor) {
-			const sourceCode = editor.document.getText();
-			tree = parser.parse(sourceCode);
+class StringEnterTypingAssist implements ITypingAssist {
+	isQuote = (text: string): boolean => ["'", '"', "f'", 'f"', "r'", 'r"'].includes(text);
+	isRFQuote = (text: string): boolean => ["f'", 'f"', "r'", 'r"'].includes(text);
+	isFQuote = (text: string): boolean => ["f'", 'f"'].includes(text);
+	isCursorAfterRF = (node: any, position: vscode.Position): boolean => {
+		// Check if the node has an 'f' or 'r' prefix
+		if (this.isRFQuote(node.text)) {
+			// Check if the cursor is at that position
+			return position.character === node.startPosition.column + 1
+				&& node.parent.startPosition.row === position.line;
 		}
-	})
+		return false;
+	};
+	isCursorAtSecondPos = (node: any, position: vscode.Position): boolean => {
+		// Проверяем, что позиция курсора равна начальной позиции узла плюс один символ
+		return position.character === node.startPosition.column + 1
+			&& position.line === node.startPosition.row;
+	};
 
-	const isQuote = (text: string): boolean => ["'", '"', "f'", 'f"', "r'", 'r"'].includes(text);
-	const isRFQuote = (text: string): boolean => ["f'", 'f"', "r'", 'r"'].includes(text);
-	const isFQuote = (text: string): boolean => ["f'", 'f"'].includes(text);
 
-	disposable = vscode.workspace.onDidChangeTextDocument(e => {
+	apply(tree: any, editor: vscode.TextEditor, changeEvent: vscode.TextDocumentChangeEvent): void {
 
 		let position = vscode.window.activeTextEditor?.selection.active;
 
 		// проверка что это перенос строки + пробелы
-		if (e.contentChanges.length === 1 && /^\r\n(\s)*$/.test(e.contentChanges[0].text) && editor && position && e.contentChanges[0].rangeLength == 0) {
-
-			const isCursorAfterRF = (node: any, position: vscode.Position): boolean => {
-				// Check if the node has an 'f' or 'r' prefix
-				if (isRFQuote(node.text)) {
-					// Check if the cursor is at that position
-					return position.character === node.startPosition.column + 1
-						&& node.parent.startPosition.row === position.line;
-				}
-				return false;
-			};
-			const isCursorAtSecondPos = (node: any, position: vscode.Position): boolean => {
-				// Проверяем, что позиция курсора равна начальной позиции узла плюс один символ
-				return position.character === node.startPosition.column + 1
-					&& position.line === node.startPosition.row;
-			};
+		if (changeEvent.contentChanges.length === 1 && /^\r\n(\s)*$/.test(changeEvent.contentChanges[0].text) && editor && position && changeEvent.contentChanges[0].rangeLength == 0) {
 
 			const currentNode = tree.rootNode.descendantForPosition({
 				row: position?.line,
@@ -67,25 +41,25 @@ export async function activate(context: vscode.ExtensionContext) {
 				(
 					// нода кавычка
 					currentNode.typeId === 100
-					&& isQuote(currentNode.parent.firstChild.text)
+					&& this.isQuote(currentNode.parent.firstChild.text)
 					&& !(
 						currentNode.parent.startPosition.row === position.line
 						&& currentNode.parent.startPosition.column === position.character
 					)
-					&& !isCursorAfterRF(currentNode, position)
+					&& !this.isCursorAfterRF(currentNode, position)
 				) || (
 					// нода строка
 					currentNode.typeId === 200
-					&& isQuote(currentNode.firstChild.text)
+					&& this.isQuote(currentNode.firstChild.text)
 				) || (
 					// нода фигурная скобка
 					currentNode.typeId === 82
-					&& isFQuote(currentNode?.parent?.parent?.firstChild.text)
+					&& this.isFQuote(currentNode?.parent?.parent?.firstChild.text)
 				) || (
 					// нода спец знак
 					currentNode.typeId === 86
-					&& isQuote(currentNode?.parent?.firstChild.text)
-					&& !isCursorAtSecondPos(currentNode, position)
+					&& this.isQuote(currentNode?.parent?.firstChild.text)
+					&& !this.isCursorAtSecondPos(currentNode, position)
 				)
 			) {
 
@@ -107,7 +81,7 @@ export async function activate(context: vscode.ExtensionContext) {
 				}
 
 				const ofs1 = editor.document.offsetAt(position)
-				const ofs2 = ofs1 + e.contentChanges[0].text.length
+				const ofs2 = ofs1 + changeEvent.contentChanges[0].text.length
 				const pos1 = position;
 				const pos2 = editor.document.positionAt(ofs2);
 
@@ -123,6 +97,76 @@ export async function activate(context: vscode.ExtensionContext) {
 
 		}
 
+
+
+	}
+}
+
+// class HalloWordTypingAssist implements ITypingAssist {
+//     apply(content: string, tree: any, editor: vscode.TextEditor, changeEvent: vscode.TextDocumentChangeEvent): void {
+//         // Логика применения помощи при наборе хеловов
+//     }
+// }
+
+class TypeAssistService {
+	assistList: ITypingAssist[];
+
+	constructor() {
+		this.assistList = [
+			new StringEnterTypingAssist(),
+			// new HalloWordTypingAssist(),
+		];
+	}
+
+	processing(tree: any, editor: vscode.TextEditor, changeEvent: vscode.TextDocumentChangeEvent): void {
+		for (const assist of this.assistList) {
+			assist.apply(tree, editor, changeEvent);
+		}
+	}
+}
+
+
+
+let disposable: vscode.Disposable | undefined;
+
+export async function activate(context: vscode.ExtensionContext) {
+
+	const Parser = require('web-tree-sitter');
+	await Parser.init();
+	const parser = new Parser;
+
+	const path = require('path');
+
+	const wasmPath = path.join(__dirname, 'lib/tree-sitter-python.wasm');
+	const Python = await Parser.Language.load(wasmPath);
+	parser.setLanguage(Python);
+
+	let editor = vscode.window.activeTextEditor;
+	let tree: any;
+	if (editor) {
+		const sourceCode = editor.document.getText();
+		tree = parser.parse(sourceCode);
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(e => {
+		editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const sourceCode = editor.document.getText();
+			tree = parser.parse(sourceCode);
+		}
+	})
+
+
+
+	const assistService = new TypeAssistService();
+
+	disposable = vscode.workspace.onDidChangeTextDocument(e => {
+
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			assistService.processing(tree, editor, e);
+		}
 
 
 		const content = e.document.getText();
